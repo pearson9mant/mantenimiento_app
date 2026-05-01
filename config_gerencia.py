4e0b48a1b26fbe915b1a127e86bd1b6310ad15cb
pearson9mant <jalmedac@abatoliba.edu>
2026-05-01 08:55:23 +0200

    Update config_gerencia.py

--- config_gerencia.py
+++ config_gerencia.py
@@ -1,1 +1,31 @@
+TIPOS_SOLICITANTE = [
+    "Profesores",
+    "Operarios",
+    "Dirección",
+    "Cap de estudio",
+]
 
+ESTADOS_HECHAS = [
+    "Finalizada",
+    "Finalizado",
+    "Cerrada",
+    "Cerrado",
+]
+
+ESTADOS_EN_PROCESO = [
+    "En curso",
+    "En proceso",
+]
+
+ESTADOS_FALTAN = [
+    "Abierta",
+    "Pendiente",
+    "Pendiente material",
+    "Esperando material",
+]
+
+MOSTRAR_MESES = True
+MOSTRAR_CENTROS = True
+MOSTRAR_INVENTARIO = True
+
+STOCK_BAJO = 2
