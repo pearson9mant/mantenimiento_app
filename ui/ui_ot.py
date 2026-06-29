9e8dc2c0a5fa4c48c1ee9a59794ab8ed13dfb5e5
pearson9mant <jalmedac@abatoliba.edu>
2026-06-29 18:10:23 +0200

    Create ui_ot.py

---
+++ ui/ui_ot.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def mostrar_tarjeta_ot(
+    fila,
+    materiales_select,
+    operario_sel,
+    modo="operario"
+):
+    """
+    Tarjeta única de Orden de Trabajo.
+
+    Será utilizada por:
+    - Operario
+    - Colegio
+    - Administración
+    - Preventivos
+    - Legionella
+    """
+    pass
