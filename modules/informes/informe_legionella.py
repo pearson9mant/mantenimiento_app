703a06da1a0550921c96448aa3e160567c5579a7
pearson9mant <jalmedac@abatoliba.edu>
2026-07-18 06:30:44 +0200

    Update informe_legionella.py

--- modules/informes/informe_legionella.py
+++ modules/informes/informe_legionella.py
@@ -15,3 +15,4 @@ from reportlab.platypus import (
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import A4
+from database.db import conectar
