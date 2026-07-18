a0a1bee83b4f83540b729bca97a25cb055dc09fa
pearson9mant <jalmedac@abatoliba.edu>
2026-07-18 06:28:16 +0200

    Create informe_legionella.py

---
+++ modules/informes/informe_legionella.py
@@ -0,0 +1,17 @@
+from io import BytesIO
+from datetime import datetime
+
+import pandas as pd
+import streamlit as st
+
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Table,
+    TableStyle,
+)
+
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import A4
